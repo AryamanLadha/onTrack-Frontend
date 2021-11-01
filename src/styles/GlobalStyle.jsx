f39f322cc5372270a2e6f209ca62c79f17acb428
jenehojinchoi <jenehojinchoi@gmail.com>
2021-11-01 10:54:53 -0700

    initial commit

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  html{
+    font-size: 10px;
+  }
+  @media screen and (max-width: 1440px) {
+    html {
+      font-size: 8px;
+    }
+  }
+  @media screen and (max-width: 1024px) {
+    html {
+      font-size: 6px;
+    }
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+    font-family: "Noto Sans kr", "Spoqa Han Sans Neo",
+      -apple-system, BlinkMacSystemFont, "Roboto",
+    padding: 0;
+    margin: 0;
+    max-height: 100vh;
+    overflow: overlay;
+    -ms-overflow-style: none;
+    
+    ::-webkit-scrollbar {
+      width: 6px;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 6px;
+      background-color: rgba(223, 223, 223, 0.6);
+    
+      &:hover {
+        background-color: rgba(193, 193, 193, 0.6);
+      }
+    }
+  };
+  a { color: #fff; text-decoration: none; outline: none }
+  a:hover, a:active { text-decoration: none; color:#fff; background-color: transparent; }
+  button{ outline: none; background: none; border: none; cursor: pointer; };
+  input { border: none; outline: none; }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.jsx
@@ -0,0 +1,24 @@
+//how to use
+//font: ${({ theme }) => theme.font.display1};
+// color: ${({ theme }) => theme.color.primary};
+
+const color = {
+    // colors
+  };
+  
+  /**
+   * bold: 700
+   * medium: 600
+   * regular: 400
+   * light: 300
+   */
+  const font = {
+    // fonts 
+  };
+  
+  const theme = {
+    color,
+    font,
+  };
+  
+  export default theme;
