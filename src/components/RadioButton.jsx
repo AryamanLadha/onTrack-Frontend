c1a08d3244c29f037f91c54e88d33df9a8952b65
jenehojinchoi <jenehojinchoi@gmail.com>
2021-11-18 14:57:50 -0800

    style: add Typography

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,64 +1,45 @@
 import * as React from "react"
-//import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import RadioGroup from '@mui/material/RadioGroup';
 import Radio from '@mui/material/Radio';
+import Typography from '@mui/material/Typography';
 import { makeStyles } from "@mui/styles"
 
 const useStyles = makeStyles((theme) => ({
     freshman: {
         font: theme.font.subtitle,
-        color: theme.color.lightgrey, 
+        backgroundColor: theme.color.lightgrey, 
     },
-
     Laura: {
         font: theme.font.subtitle,
     },
-/*
-    formLabel: {
-        font: theme.font.subtitle,
-    }
-
-    freshman: {
-        theme: theme.font.subtitle,
-    }
-
-    transfer: {
-
-    }
-    */
-
 }));
-
-
-//makestyles: radio button form is its own class too 
 
 export default function RadioButton() 
 {
-   const classes = useStyles();
-
-    return(
-
+  const classes = useStyles();
+  return(
     <FormControl 
-        //className = {classes.formControl}
-        component="fieldset"
+      component="fieldset"
     >
-
-    <RadioGroup
-        //aria-label="Year Entered"
-       // defaultValue="Freshman"
+      <RadioGroup
         name="radio-buttons-group"
-       // className = {classes.radioGroup}
-    >
-
-        <div> Laura </div> 
-        <FormControlLabel value="Freshman" control={<Radio />} label="Freshman" className = {classes.freshman} />
-        <FormControlLabel value="Transfer" control={<Radio />} label="Transfer"/>
-    </RadioGroup>
-
+      >
+        <div className={classes.Laura}> Laura </div> 
+        <FormControlLabel   
+          value="Freshman" 
+          control={<Radio />} 
+          label={<Typography variant="caption">Freshman</Typography>} 
+        />
+        <FormControlLabel 
+          value="Transfer" 
+          control={<Radio />} 
+          label={<Typography variant="caption">Transfer</Typography>} 
+        />
+      </RadioGroup>
     </FormControl>
-    );
+  );
 }
 
 
