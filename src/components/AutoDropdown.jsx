5066e65254e02929e42428fd6c287d842fea8b21
Ollie Pai <olliepai@Ollies-MacBook-Pro.local>
2021-11-20 19:21:11 -0800

    feat: auto dropdown (wip), sorry jean

--- src/components/AutoDropdown.jsx
+++ src/components/AutoDropdown.jsx
@@ -2,15 +2,46 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
-const options = ['The Godfather', 'Pulp Fiction'];
+const displayData = [
+  { name: "African American Studies" },
+  { name: "African and Middle Eastern Studies" },
+  { name: "American Indian Studies" },
+  { name: "American Literature and Culture" },
+  { name: "Ancient Near East and Egyptology" },
+];
+
+let selected = [];
 
 export default function AutoDropdown() {
+  const [value, setValue] = React.useState("");
+
   return (
-    <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={options}
-      renderInput={(params) => <TextField {...params} label="Movie" />}
-    />
+    <div>
+      {selected.map((itemName) => {
+        return <div>{itemName}</div>;
+      })}
+      <h1>Enter your major(s)</h1>
+      <br />
+      <Autocomplete
+        multiple
+        id="tags-standard"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          selected.map
+        }}
+        options={displayData}
+        getOptionLabel={(option) => option.name}
+        //defaultValue={[option[0]]}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Majors"
+            //placeholder="Favorites"
+          />
+        )}
+      />
+    </div>
   );
 }
