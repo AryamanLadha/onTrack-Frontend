35ef1039ea1bf0ffc2da6c121d34a19f46c0d908
Ollie Pai <olliepai@Ollies-MacBook-Pro.local>
2021-11-24 01:39:29 -0800

    feat: what year are you page

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as PageButton } from './PageButton';
 export { default as CourseCard } from './CourseCard';
 export { default as SearchBar } from './SearchBar';
 export { default as RadioButton } from './RadioButton';
+export { default as Dropdown } from './Dropdown';
---
+++ src/pages/WhatYear.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+import { makeStyles } from "@mui/styles";
+import { Dropdown, RadioButton, PageButton } from "../components";
+
+const useStyles = makeStyles(theme => ({
+  layout: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "100vw",
+  },
+
+  header: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    width: "100rem", 
+    height: "8.5rem",
+    marginTop: "5rem",
+    marginBottom: "4.9rem",
+  },
+
+  title: {
+    font: theme.font.title,
+    color: theme.color.black, 
+    textAlign: "center",
+    margin: "0 0 0 0.9rem",
+    fontWeight: "bold",
+  },
+
+  subtitle: {
+    textAlign: "center",
+    font: theme.font.subtitle,
+    marginTop: "1rem",
+  },
+
+  spacer: {
+    height: "6.5rem",
+  },
+}));
+
+export default function WhatYear() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.layout}>
+      <header className={classes.header}>
+				<h1 className={classes.title}>
+          When did you enter UCLA?
+        </h1>
+        <div className={classes.subtitle}>
+        We're all counting down the days till graduation.
+        </div>
+			</header>
+      <div>
+        <div
+          className={classes.subtitle}
+          style={{
+            float: "left",
+            width: "30%",
+            padding: "1rem 2rem 0rem 3rem",
+            marginLeft: "-6rem",
+          }}
+        >
+          Start
+        </div>
+        <div
+          style={{
+            float: "left",
+            width: "70%",
+          }}
+        >
+          <Dropdown
+            placeholder="Select a quarter"
+            options={[
+              "Fall 2018",
+              "Winter 2019",
+              "Sping 2019",
+            ]}
+          />
+        </div>
+      </div>
+      <div className={classes.spacer}></div>
+      <div>
+        <div
+          className={classes.subtitle}
+          style={{
+            float: "left",
+            width: "30%",
+            padding: "1rem 2rem 0rem 3rem",
+            marginLeft: "-6rem",
+          }}
+        >
+          End
+        </div>
+        <div
+          style={{
+            float: "left",
+            width: "70%",
+          }}
+        >
+          <Dropdown
+            placeholder="Select a quarter"
+            options={[
+              "Fall 2018",
+              "Winter 2019",
+              "Sping 2019",
+            ]}
+          />
+        </div>
+      </div>
+      <div className={classes.spacer}></div>
+      <div  style={{ marginLeft: "-28rem" }}>
+      <div
+          className={classes.subtitle}
+          style={{
+            float: "left",
+            width: "60%",
+            padding: "1rem 2rem 0rem 3rem",
+            //marginLeft: "-20rem",
+          }}
+        >
+          Entered as a:
+        </div>
+        <div
+          style={{
+            float: "left",
+            width: "40%",
+          }}
+        >
+          <RadioButton />
+        </div>
+      </div>
+      <div style={{ height: "20rem" }}></div>
+      <div>
+        <div
+          style={{
+            float: "right",
+            width: "10%",
+          }}
+        >
+          <PageButton
+            text="Next"
+            size="short"
+          />
+        </div>
+        <div
+          style={{
+            float: "left",
+            width: "10%",
+          }}
+        >
+          <PageButton
+            text="Back"
+            size="short"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
