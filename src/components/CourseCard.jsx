759d733f091ea4ce4ea18e8cc73b6c0ae2d2da84
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2022-01-28 21:29:52 -0800

    feat: made CourseCard backgroundColor darker onHover

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,35 +1,48 @@
-import React from 'react';
-import { makeStyles } from '@mui/styles';
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles (theme => ({
-  card: {
-    backgroundColor: theme.color.grey,
-    width: '17.5rem',
-    height: '17.5rem',
-    borderRadius: '2.5rem',
-    textAlign: 'center',
-    lineHeight: '17.5rem',
-    font: theme.font.courseCard,
-  },
+const useStyles = (isHovering) =>
+  makeStyles((theme) => ({
+    card: {
+      backgroundColor: isHovering ? "#28313e" : theme.color.grey,
+      width: "17.5rem",
+      height: "17.5rem",
+      borderRadius: "2.5rem",
+      textAlign: "center",
+      lineHeight: "17.5rem",
+      font: theme.font.courseCard,
+    },
 
-  cardText: {
-    display: 'inline-block',
-    verticalAlign: 'middle',
-    lineHeight: '2.346rem',
-  },
-}));
+    cardText: {
+      display: "inline-block",
+      verticalAlign: "middle",
+      lineHeight: "2.346rem",
+    },
+  }));
 
-const CourseCard = ({ name, selectedCourses, setSelectedCourses}) => {
-  const classes = useStyles();; 
+const CourseCard = ({ name, selectedCourses, setSelectedCourses }) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const classes = useStyles(isHovering)();
+
   const handleClick = () => {
-    setSelectedCourses(selectedCourses.filter(element => element !== name));
-  }
+    setSelectedCourses(selectedCourses.filter((element) => element !== name));
+  };
+
+  const handleHover = () => {
+    setIsHovering(!isHovering);
+  };
 
   return (
-    <div className = {classes.card} onClick={handleClick}> 
+    <div
+      className={classes.card}
+      onClick={handleClick}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+    >
       <span className={classes.cardText}>{name}</span>
     </div>
   );
-}
+};
 
 export default CourseCard;
