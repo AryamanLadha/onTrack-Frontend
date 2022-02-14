f987669883ba887018b2f1f9e016f34441e605a5
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2022-02-13 17:40:24 -0800

    built frozen popup overlay

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -11,17 +11,23 @@ const theme = createTheme({
     subtitle: 'normal 400 2rem/2rem "Work Sans"',
     radioButton: 'normal 400 2rem/2rem "Work Sans" !important',
     accordionDropdown: 'normal 500 3rem/3rem "Work Sans" !important',
+    eligibleCourseOverlayHeader: 'normal 700 8.125rem/9.53rem "Work Sans"',
+    eligibleCourseOverlaySubheader: 'normal 500 4.40rem/3.75rem "Work Sans"',
+    eligibleCourseTagText: 'normal 400 2.5rem/2.94rem "Work Sans"',
+    eligibleCourseDescriptionText: 'normal 400 2.25rem/2.64rem "Work Sans"',
+    eligibleCourseTableHeaderText: 'normal 500 2.5rem/2.93rem "Work Sans"',
+    eligibleCourseTableRowText: 'normal 400 2.5rem/2.93rem "Work Sans"',
   },
   color: {
-    button: "#C4C4C4",
-    grey: "#C4C4C4 !important",
-    black: "#000000",
-    lightgrey: "#E7E7E7 !important",
-    icongrey: "#A3A3A3",
-    bargrey: "#EDEDED",
-    white: "#000000",
-  }
-})
+    button: '#C4C4C4',
+    grey: '#C4C4C4 !important',
+    black: '#000000',
+    lightgrey: '#E7E7E7 !important',
+    icongrey: '#A3A3A3',
+    bargrey: '#EDEDED',
+    white: '#FFFFFF',
+  },
+});
 
 /**
  * bold: 700
