309c679bad5568e9055fe685c011ff1b26d29b48
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2022-02-20 10:30:36 -0800

    feat: built base of eligible course info popdown

--- src/pages/EligibleCourses.jsx
+++ src/pages/EligibleCourses.jsx
@@ -3,9 +3,7 @@ import { connect } from 'react-redux';
 import { makeStyles } from '@mui/styles';
 import { AccordionDropdown, PageButton } from '../components';
 import { getEligible } from '../actions/actions';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import { styled } from '@mui/material/styles';
+import TriangleWarning from '../assets/icons/TriangleWarning.svg';
 
 const useStyles = makeStyles((theme) => ({
   layout: {
@@ -46,6 +44,54 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
     flexDirection: 'row',
     marginTop: '7.4rem',
+  },
+
+  popDown: {
+    width: '117.5rem',
+    backgroundColor: '#D4D4D4',
+    borderRadius: '5.5rem',
+    padding: '6.25rem 3.75rem',
+  },
+
+  courseTitle: {
+    font: theme.font.eligibleCourseOverlayHeader,
+    marginBottom: '2.5rem',
+  },
+
+  courseTags: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '1rem 2.5rem',
+    marginBottom: '3.25rem',
+  },
+
+  tag: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '0rem 2rem',
+    backgroundColor: theme.color.grey,
+    padding: '0rem 2.5rem',
+    borderRadius: '2rem',
+    height: '6.25rem',
+  },
+
+  warningIcon: {
+    width: '4.375rem',
+    height: '4.375rem',
+  },
+
+  tagText: {
+    font: theme.font.eligibleCourseTagText,
+  },
+
+  courseDescriptionHeader: {
+    font: theme.font.eligibleCourseDescriptionText,
+    marginBottom: '0.25rem',
+  },
+
+  courseDescriptionBody: {
+    font: theme.font.eligibleCourseDescriptionText,
+    marginBottom: '2rem',
   },
 }));
 
@@ -95,6 +141,44 @@ function EligibleCourses({
         <div className={classes.pageButtonWrapper}>
           <PageButton text="Back" size="short" page="eligible" />
           <PageButton text="Next" size="short" page="eligible" />
+        </div>
+
+        <div className={classes.popDown}>
+          <div className={classes.courseTitle}>Name of Course</div>
+
+          <div className={classes.courseTags}>
+            <div className={classes.tag}>
+              <img
+                src={TriangleWarning}
+                className={classes.warningIcon}
+                alt="enrollmentWarning"
+              />
+
+              <div className={classes.tagText}>Enrollment Restrictions</div>
+            </div>
+
+            <div className={classes.tag}>
+              <img
+                src={TriangleWarning}
+                className={classes.warningIcon}
+                alt="enrollmentWarning"
+              />
+
+              <div className={classes.tagText}>Corequisites</div>
+            </div>
+          </div>
+
+          <div className={classes.courseDescriptionHeader}>
+            Course Description:
+          </div>
+
+          <div className={classes.courseDescriptionBody}>
+            Lecture, three hours. Exploration of computer metaphor of mind as an
+            information-processing system, focusing especially on perception,
+            knowledge representation, and thought based on research in cognitive
+            psychology, neuropsychology, and artificial intelligence. Many
+            examples from visual information processing.
+          </div>
         </div>
       </div>
     </>
