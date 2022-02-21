25ba067dae8ed545b431a68dc5dafc0917d7df63
jenehojinchoi <jenehojinchoi@gmail.com>
2022-02-20 18:59:45 -0800

    style: styling

--- src/components/SelectCourseDropdown.jsx
+++ src/components/SelectCourseDropdown.jsx
@@ -10,12 +10,12 @@ import { MiniCourseCard } from "./index";
 const useStyles = (props) => makeStyles((theme) => ({
   accordionDropdown: {
     width: "100%",
-    height: "7.2rem",
+    height: "6rem",
     flexDirection: "row-reverse",
     boxShadow: "none",
     border: "0.1rem solid",
     borderRadius: props.expanded ? "1.5rem 1.5rem 0rem 0rem !important" : "1.5rem !important",
-    borderColor: theme.color.lightBeige,
+    borderColor: theme.color.darkBeige,
   },
 
   typography: {
@@ -34,7 +34,15 @@ const useStyles = (props) => makeStyles((theme) => ({
     },
 
     "& .MuiAccordionDetails-root" : {
+      position: "absolute",
+      width: "84.8rem",
+      top: "7rem",
+      left: "-0.1rem",
       marginBottom: "0rem",
+      border: "0.1rem solid",
+      borderRadius: "0rem 0rem 1.5rem 1.5rem",
+      borderColor: theme.color.darkBeige,
+      background: theme.color.white,
     }
   },
 
@@ -118,17 +126,17 @@ const SelectCourseDropdown = ({ data, overlayOpened, setOverlayOpened, setQuarte
           <div className={classes.miniCourseCardWrapper}>
             {[...Array(numRows).keys()].map((i) => (
               <div key={i} className={classes.miniCourseCardContainer}>
-                {data.courses.slice(i*5, i*5+5).map((course, idx) => 
-                  <MiniCourseCard key={idx} name={course} />
-                )}
-                {((i === numRows-1 && data.courses.length !== 0) || data.courses.length === 0) && 
-                  <MiniCourseCard 
-                    quarter={data.quarter}
-                    overlayOpened={overlayOpened}
-                    setOverlayOpened={setOverlayOpened}
-                    setQuarterOfOverlay={setQuarterOfOverlay}
-                  />}
-              </div>
+              {data.courses.slice(i*5, i*5+5).map((course, idx) => 
+                <MiniCourseCard key={idx} name={course} />
+              )}
+              {((i === numRows-1 && data.courses.length !== 0) || data.courses.length === 0) && 
+                <MiniCourseCard 
+                  quarter={data.quarter}
+                  overlayOpened={overlayOpened}
+                  setOverlayOpened={setOverlayOpened}
+                  setQuarterOfOverlay={setQuarterOfOverlay}
+                />}
+            </div>
             ))}
           </div>
         </AccordionDetails> 
