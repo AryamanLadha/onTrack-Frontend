cab4662cfcdad5c2c10477cc97989b1575afca82
Ollie Pai <ollie.pai2003@gmail.com>
2022-02-22 00:25:25 -0800

    created empty pages for Login, Profile, and EditProfile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,7 @@
-export { default as EnterCoursesByQuarter } from './EnterCoursesByQuarter';
+export { default as EditProfile } from './EditProfile';
 export { default as EligibleCourses } from './EligibleCourses';
+export { default as EnterCoursesByQuarter } from './EnterCoursesByQuarter';
+export { default as Login } from './Login';
+export { default as Profile } from './Profile';
 export { default as WhatMajor } from './WhatMajor';
 export { default as WhatYear } from './WhatYear';
