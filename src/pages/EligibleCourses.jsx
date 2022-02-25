5eba8914478a5d03cdfa061fb99f543a0618db60
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2022-02-25 13:45:35 -0800

    fix: made page rerender after async API call finishes returning

--- src/pages/EligibleCourses.jsx
+++ src/pages/EligibleCourses.jsx
@@ -37,10 +37,10 @@ const useStyles = makeStyles((theme) => ({
     font: theme.font.eligibleCourseQuarter,
     background: theme.color.white,
     borderColor: theme.color.darkBeige,
-    border: "solid 0.1rem",
-    borderRadius: "1.5rem",
-    padding: "1.5rem 2rem",
-    margin: "2rem",
+    border: 'solid 0.1rem',
+    borderRadius: '1.5rem',
+    padding: '1.5rem 2rem',
+    margin: '2rem',
   },
 
   pageButtonWrapper: {
@@ -82,40 +82,49 @@ function EligibleCourses({
   eligibleCoursesData,
   storeMajors,
   storeCoursesTaken,
-
 }) {
   const classes = useStyles();
-  const [ nextQuarter, setNextQuarter ] = useState("");
+
+  const [nextQuarter, setNextQuarter] = useState('');
+
   // 1. When page renders, create an object to hold display data, uses data pulled from the store (majors and coursesTaken). See mapStateToProps below.
   // 2. Then dispatch getEligible action to get the list of courses to display on this page (which will be stored in currentClasses portion of studentData object)
   useEffect(() => {
     // See API Docs for more detail about course object structure: https://docs.google.com/document/d/1K_EwdJnraRhgYYT1dDU4aiw_GFCbMcqSNi7-EAOIdJA/edit?usp=sharing
     let completed = [];
-    storeCoursesTaken.map(object => (
-      completed.push(...object.courses)
-    ));
+
+    storeCoursesTaken.map((object) => completed.push(...object.courses));
+
     const studentData = {
       major: storeMajors,
       completedClasses: completed,
       currentClasses: [],
     };
+
     studentData && getEligible(JSON.stringify(studentData));
-    eligibleCoursesData !== undefined && setNextQuarter(eligibleCoursesData[0].quarter)
 
     // eslint-disable-next-line
   }, []);
-  
+
+  useEffect(() => {
+    if (eligibleCoursesData.length !== 0) {
+      setNextQuarter(eligibleCoursesData[0].quarter);
+    }
+  }, [eligibleCoursesData]);
+
   return (
     <>
       <div className={classes.layout}>
         <header className={classes.header}>
           <h1 className={classes.title}>Eligible Courses</h1>
-          <div className={classes.quarter}>{nextQuarter}</div>
+
+          {eligibleCoursesData.length !== 0 && (
+            <div className={classes.quarter}>{nextQuarter}</div>
+          )}
         </header>
         <div>
           {/* Displaying grid of miniCourseCards. We parse data from the course object to do this. */}
-          {eligibleCoursesData.length !== 0 
-          ? (
+          {eligibleCoursesData.length !== 0 ? (
             eligibleCoursesData.map((eligibleCourse, idx) => (
               <div className={classes.miniCourseCardWrapper}>
                 {Object.entries(eligibleCourse.subjects).map(([key, value]) => (
@@ -138,7 +147,8 @@ function EligibleCourses({
           <PageButton text="Back" size="short" page="eligible" />
           <PageButton text="Next" size="short" page="eligible" />
         </div>
-        <CourseInfoPopdown />
+
+        {/* <CourseInfoPopdown /> */}
       </div>
     </>
   );
